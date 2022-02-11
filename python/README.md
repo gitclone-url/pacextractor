520eec584a99e4902243a540328f598eac376c5e
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2022-02-11 11:19:35 +0530

    Use crcmod module for faster crc calculation * also show progress while calculating crc

---
+++ python/README.md
@@ -0,0 +1,19 @@
+### Python PacExtractor
+
+#### Install Requirements
+`pip3 install crcmod`
+
+
+#### Usage
+```
+usage: pacExtractor.py [-h] [-d] [-c] pacfile [outdir]
+
+positional arguments:
+  pacfile     Spreadtrum .pac file
+  outdir      output directory to extract files
+
+options:
+  -h, --help  show this help message and exit
+  -d          enable debug output
+  -c          compute and verify CRC16
+```
--- python/pacExtractor.py
+++ python/pacExtractor.py
@@ -11,16 +11,17 @@
 
 # TODO: Add support for 4GB+ .pac firmware
 
-import argparse
-import os
-import struct
-import sys
+import argparse, os, struct, sys
+import crc16
 
-
+# 2124 bytes = (24*2)+4+(256*2)+(256*2)+4+4+4+4+4+4+4+(100*2)+4+4+4+(800*1)+4+2+2
 PAC_HEADER_FMT = '48s I 512s 512s I I I I I I I 200s I I I 800s I H H'
+
+# 2580 byte = 4+(256*2)+(256*2)+(256*2)+4+4+4+4+4+4+(5*4)+(996*1)
 FILE_HEADER_FMT = 'I 512s 512s 512s I I I I I I 5I 996s'
+
 PAC_MAGIC = '0xfffafffa'
-fiveSpaces = "     "
+fiveSpaces = ' ' * 5
 
 PAC_HEADER = {
     'szVersion': '',           # packet struct version
@@ -63,48 +64,6 @@ FILE_HEADER = {
     'dwReserved': 0            # Reserved for future, not used now
 }
 
-CRC16_TABLE = [
-    0x0000, 0xc0c1, 0xc181, 0x0140, 0xc301, 0x03c0, 0x0280, 0xc241,
-    0xc601, 0x06c0, 0x0780, 0xc741, 0x0500, 0xc5c1, 0xc481, 0x0440,
-    0xcc01, 0x0cc0, 0x0d80, 0xcd41, 0x0f00, 0xcfc1, 0xce81, 0x0e40,
-    0x0a00, 0xcac1, 0xcb81, 0x0b40, 0xc901, 0x09c0, 0x0880, 0xc841,
-    0xd801, 0x18c0, 0x1980, 0xd941, 0x1b00, 0xdbc1, 0xda81, 0x1a40,
-    0x1e00, 0xdec1, 0xdf81, 0x1f40, 0xdd01, 0x1dc0, 0x1c80, 0xdc41,
-    0x1400, 0xd4c1, 0xd581, 0x1540, 0xd701, 0x17c0, 0x1680, 0xd641,
-    0xd201, 0x12c0, 0x1380, 0xd341, 0x1100, 0xd1c1, 0xd081, 0x1040,
-    0xf001, 0x30c0, 0x3180, 0xf141, 0x3300, 0xf3c1, 0xf281, 0x3240,
-    0x3600, 0xf6c1, 0xf781, 0x3740, 0xf501, 0x35c0, 0x3480, 0xf441,
-    0x3c00, 0xfcc1, 0xfd81, 0x3d40, 0xff01, 0x3fc0, 0x3e80, 0xfe41,
-    0xfa01, 0x3ac0, 0x3b80, 0xfb41, 0x3900, 0xf9c1, 0xf881, 0x3840,
-    0x2800, 0xe8c1, 0xe981, 0x2940, 0xeb01, 0x2bc0, 0x2a80, 0xea41,
-    0xee01, 0x2ec0, 0x2f80, 0xef41, 0x2d00, 0xedc1, 0xec81, 0x2c40,
-    0xe401, 0x24c0, 0x2580, 0xe541, 0x2700, 0xe7c1, 0xe681, 0x2640,
-    0x2200, 0xe2c1, 0xe381, 0x2340, 0xe101, 0x21c0, 0x2080, 0xe041,
-    0xa001, 0x60c0, 0x6180, 0xa141, 0x6300, 0xa3c1, 0xa281, 0x6240,
-    0x6600, 0xa6c1, 0xa781, 0x6740, 0xa501, 0x65c0, 0x6480, 0xa441,
-    0x6c00, 0xacc1, 0xad81, 0x6d40, 0xaf01, 0x6fc0, 0x6e80, 0xae41,
-    0xaa01, 0x6ac0, 0x6b80, 0xab41, 0x6900, 0xa9c1, 0xa881, 0x6840,
-    0x7800, 0xb8c1, 0xb981, 0x7940, 0xbb01, 0x7bc0, 0x7a80, 0xba41,
-    0xbe01, 0x7ec0, 0x7f80, 0xbf41, 0x7d00, 0xbdc1, 0xbc81, 0x7c40,
-    0xb401, 0x74c0, 0x7580, 0xb541, 0x7700, 0xb7c1, 0xb681, 0x7640,
-    0x7200, 0xb2c1, 0xb381, 0x7340, 0xb101, 0x71c0, 0x7080, 0xb041,
-    0x5000, 0x90c1, 0x9181, 0x5140, 0x9301, 0x53c0, 0x5280, 0x9241,
-    0x9601, 0x56c0, 0x5780, 0x9741, 0x5500, 0x95c1, 0x9481, 0x5440,
-    0x9c01, 0x5cc0, 0x5d80, 0x9d41, 0x5f00, 0x9fc1, 0x9e81, 0x5e40,
-    0x5a00, 0x9ac1, 0x9b81, 0x5b40, 0x9901, 0x59c0, 0x5880, 0x9841,
-    0x8801, 0x48c0, 0x4980, 0x8941, 0x4b00, 0x8bc1, 0x8a81, 0x4a40,
-    0x4e00, 0x8ec1, 0x8f81, 0x4f40, 0x8d01, 0x4dc0, 0x4c80, 0x8c41,
-    0x4400, 0x84c1, 0x8581, 0x4540, 0x8701, 0x47c0, 0x4680, 0x8641,
-    0x8201, 0x42c0, 0x4380, 0x8341, 0x4100, 0x81c1, 0x8081, 0x4040
-]
-
-
-# calculate/update CRC16
-def calc_crc16(crc_base, s):
-    for b in s:
-        crc_base = (crc_base >> 8) ^ (CRC16_TABLE[(crc_base ^ b) & 0xff])
-    return crc_base
-
 
 def abort(message):
     print(message, file=sys.stderr)
@@ -112,7 +71,7 @@ def abort(message):
 
 
 def getString(name):
-    return name.decode('utf-16').rstrip('\x00')
+    return name.decode('utf-16le').rstrip('\x00')
 
 
 def printP(name, value):
@@ -145,13 +104,11 @@ def parsePacHeader(f, pacfile, debug):
     pacHeader = PAC_HEADER.copy()
     pacHeaderBin = struct.unpack(PAC_HEADER_FMT, f.read(struct.calcsize(PAC_HEADER_FMT)))
 
-    i = 0
-    for k, v in pacHeader.items():
+    for i, (k, v) in enumerate(pacHeader.items()):
         if str(v) == '0':
             pacHeader[k] = pacHeaderBin[i]
         else:
             pacHeader[k] = getString(pacHeaderBin[i])
-        i += 1
 
     if pacHeader['szVersion'] != 'BP_R1.0.0':
         abort('Unsupported PAC version')
@@ -169,29 +126,30 @@ def verifyCRC16(f, ph, debug):
         print('Checking CRC Part 1')
         f.seek(0)
         crcbuf = f.read(struct.calcsize(PAC_HEADER_FMT) - 4)
-        crc1val = calc_crc16(0, crcbuf)
+        crc1val = crc16.crc16(crcbuf)
         if crc1val != ph['wCRC1']:
             if debug:
                 print(f'Computed CRC1 = {crc1val}, CRC1 in PAC = {ph["wCRC1"]}')
-            abort("CRC Check failed for CRC1\n")
+            abort('CRC Check failed for CRC1\n')
 
     print('Checking CRC Part 2')
     f.seek(struct.calcsize(PAC_HEADER_FMT))
     bufsize = 64 * 1024
-    totallen = ph['dwSize'] - struct.calcsize(PAC_HEADER_FMT)
+    tempsize = ph['dwSize'] - struct.calcsize(PAC_HEADER_FMT)
+    tsize = tempsize
     crc2val = 0
-    while totallen > 0:
-        if totallen < bufsize:
-            bufsize = totallen
+    while tempsize > 0:
+        if tempsize < bufsize:
+            bufsize = tempsize
         crcbuf = f.read(bufsize)
-        totallen -= bufsize
-        crc2val = calc_crc16(crc2val, crcbuf)
+        tempsize -= bufsize
+        crc2val = crc16.crc16(crcbuf, crc2val)
+        print(f'\r{int(100 - ((100 * tempsize) / tsize))}%', end='')
+    print(f'\r{fiveSpaces}')
     if crc2val != ph['wCRC2']:
         if debug:
             print(f'Computed CRC2 = {crc2val}, CRC2 in PAC = {ph["wCRC2"]}\n')
         abort('CRC Check failed for CRC2')
-
-    print()
 
 
 def printFileHeader(fh):
@@ -210,13 +168,11 @@ def parseFiles(f, fileHeaders, debug):
     fileHeader = FILE_HEADER.copy()
     fileHeaderBin = struct.unpack(FILE_HEADER_FMT, f.read(struct.calcsize(FILE_HEADER_FMT)))
 
-    i = 0
-    for k, v in fileHeader.items():
+    for i, (k, v) in enumerate(fileHeader.items()):
         if str(v) == '0':
             fileHeader[k] = fileHeaderBin[i]
         else:
             fileHeader[k] = getString(fileHeaderBin[i])
-        i += 1
 
     if fileHeader['length'] != struct.calcsize(FILE_HEADER_FMT):
         abort('Unknown Partition Header format found')
@@ -248,9 +204,12 @@ def extractFile(f, fh, outdir):
     print(f'\r{fh["fileName"]}{fiveSpaces}')
 
 
-def main(pacfile, outdir, debug, checkCRC16):
+# main('path/to/pacfile')
+def main(pacfile, outdir=None, debug=False, checkCRC16=False):
     if os.stat(pacfile).st_size < struct.calcsize(PAC_HEADER_FMT):
         abort(f'{pacfile} is not a PAC firmware.')
+    if outdir == None:  # use 'outdir' as default output directory if None specified
+        outdir = os.path.join(os.getcwd(), 'outdir')
     if os.path.isfile(outdir):
         abort(f'file with name "{outdir}" exists')
 
@@ -264,7 +223,7 @@ def main(pacfile, outdir, debug, checkCRC16):
         if checkCRC16:
             verifyCRC16(f, pacHeader, debug)
 
-        # Unpack partition headers
+        # Unpack partition Headers
         fileHeaders = []
         f.seek(pacHeader['partitionsListStart'])
         for i in range(pacHeader['partitionCount']):
@@ -286,8 +245,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('pacfile', help='Spreadtrum .pac file')
-    parser.add_argument('outdir',nargs='?', default=os.path.join(os.getcwd(), 'outdir'),
-                        help='output directory to extract files')
+    parser.add_argument('outdir', nargs='?', help='output directory to extract files')
     parser.add_argument('-d', dest='debug', action='store_true', help='enable debug output')
     parser.add_argument('-c', dest='checkCRC16', action='store_true', help='compute and verify CRC16')
     args = parser.parse_args()
